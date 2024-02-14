ec7085a7eb892c6a534496bd1e766be827d716f1
adhan-857 <adhan.andika857@gmail.com>
2024-02-14 13:14:29 +0700

    Try fixing EshopApplicationTest to reach 100% coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,27 +1,17 @@
 package id.ac.ui.cs.advprog.eshop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class EshopApplicationTests {
 
-    @Autowired
-    private EshopApplication application;
-
     @Test
     void contextLoads() {
+        EshopApplication.main(new String[]{});
         assertTrue(true, "The context loads successfully");
     }
-
-    @Test
-    void testMain() {
-        application.main(new String[] {});
-    }
-
 }
